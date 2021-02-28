8350adc00a65fab37e1ed086b1672167aa9b3c3b
filipepoletto <filipepoletto@gmail.com>
2021-02-28 19:55:11 -0300

    :pencil: Adjusts for mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,16 +13,14 @@ function footerSubmenu() {
     const navFooterItems = navFooter.querySelectorAll('li.nav-item');
 
     for (let navFooterItem of navFooter.children) {
-        navFooterItem.addEventListener('mouseover', function (e) {
-            e.preventDefault();
+        navFooterItem.addEventListener('mouseover', function () {
             for (let navFooterItem of navFooterItems) {
                 navFooterItem.classList.remove('active');
             }
             navFooterItem.classList.add('active');
         });
     }
-    navFooter.addEventListener('mouseout', function (e) {
-        e.preventDefault();
+    navFooter.addEventListener('mouseout', function () {
         for (let navFooterItem of navFooterItems) {
             navFooterItem.classList.remove('active');
         }
